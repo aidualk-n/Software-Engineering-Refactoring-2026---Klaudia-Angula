cc0148a088f2e4e5102d12d9200cffdf8f46673c
aidualk-n <klaudiaangula@hotmail.com>
2026-04-13 22:25:48 +0200

    Added default getFrequentRenterPoints to price

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/Price.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/Price.java
@@ -1,4 +1,9 @@
 public abstract class Price {
     public abstract int getPriceCode();
     public abstract double getCharge(int daysRented);
+
+    // Step 12.1: Add default frequent renter points method
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
