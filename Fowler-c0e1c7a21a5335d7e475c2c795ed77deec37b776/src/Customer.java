103e54d23a5d03599154961f2da61f2bc074e8cd
aidualk-n <klaudiaangula@hotmail.com>
2026-04-13 21:09:32 +0200

    Replaced totalAmount temp with query getTotalChrge()

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/Customer.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/Customer.java
@@ -25,7 +25,6 @@ class Customer {
     ;
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
@@ -37,12 +36,20 @@ class Customer {
             frequentRenterPoints += each.getFrequentRenterPoints();
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
 
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentalsEnum = rentals.elements();
+        while (rentalsEnum.hasMoreElements()) {
+            Rental each = (Rental) rentalsEnum.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
 }
